3cd0c56ca0ffc1c6d54968e67c64fbbea15bf0b9
Madhav Maheshwari1 <classwhitet@gmail.com>
2023-11-19 10:58:54 +0530

    Changes commited

---
+++ src/Components/navBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <img class = "h-[80px]" src="/Images/Shoppily Logo.svg" alt="" />
+        </div>
+    );
+}
+
+export default Navbar;
